77f863bc67df9221834b9d14b40efc28f8bd43d8
khangdnguyen1708 <kdnguyen1708@gmail.com>
2023-05-13 14:24:29 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+// import react
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
