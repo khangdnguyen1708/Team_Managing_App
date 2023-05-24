55c7cb7d37d304ff3b8a25ce36588165aceafb9e
khangdnguyen1708 <kdnguyen1708@gmail.com>
2023-05-24 11:21:49 +0700

    click home

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,7 +3,7 @@ const NotFound = () => {
     <header className="container">
       <div className="row justify-content-center mt-3 mb-4">
         <div className="col-8">
-          <h1 className="text-danger">404 - Page not found</h1>
+          <h1 className="text-danger">click Home to start</h1>
         </div>
       </div>
     </header>
